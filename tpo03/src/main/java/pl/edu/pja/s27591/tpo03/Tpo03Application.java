0286754a9cd2145d0ce5dd2496629324ef04651b
s27591 <s27591@pjwstk.edu.pl>
2024-03-20 13:59:30 +0100

    Created a project

---
+++ tpo03/src/main/java/pl/edu/pja/s27591/tpo03/Tpo03Application.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.s27591.tpo03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpo03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tpo03Application.class, args);
+    }
+
+}
