dbd5a9b716e2a023bc374e70300e206ebbcb8dea
s27591 <s27591@pjwstk.edu.pl>
2024-04-18 17:02:03 +0200

    Fixed minor bugs and added css file for better appearance

--- tpo06/src/main/java/pjwstk/edu/pl/s27591/tpo06/Tpo06Application.java
+++ tpo06/src/main/java/pjwstk/edu/pl/s27591/tpo06/Tpo06Application.java
@@ -1,6 +1,5 @@
 package pjwstk.edu.pl.s27591.tpo06;
 
-import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ public class Tpo06Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Tpo06Application.class, args);
-
     }
 
 }
--- tpo06/src/main/java/pjwstk/edu/pl/s27591/tpo06/controller/FakeDataController.java
+++ tpo06/src/main/java/pjwstk/edu/pl/s27591/tpo06/controller/FakeDataController.java
@@ -1,6 +1,7 @@
 package pjwstk.edu.pl.s27591.tpo06.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,46 +9,79 @@ import org.springframework.web.bind.annotation.PostMapping;
 import pjwstk.edu.pl.s27591.tpo06.model.FakeDataDTO;
 import pjwstk.edu.pl.s27591.tpo06.model.FakeDataService;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 @Controller
 public class FakeDataController {
     private final FakeDataService fakeDataService;
+    private final Map<String, List<String>> headers;
 
     public FakeDataController(FakeDataService fakeDataService) {
         this.fakeDataService = fakeDataService;
+        this.headers = initHeaders();
     }
 
     @GetMapping("/generateFakeData")
-    public String displayForm(Model model) {
+    public String displayForm() {
         return "fakeDataGenerator";
     }
 
     @PostMapping("/generateFakeData")
     public String displayTable(HttpServletRequest request, Model model) {
-        int num = Integer.parseInt(request.getParameter("num"));
-        String language = request.getParameter("language");
-        boolean address = request.getParameter("address") != null;
-        boolean university = request.getParameter("university") != null;
-        boolean country = request.getParameter("country") != null;
-        boolean favColor = request.getParameter("favColor") != null;
-        boolean favAnimal = request.getParameter("favAnimal") != null;
-        boolean email = request.getParameter("email") != null;
-        boolean job = request.getParameter("job") != null;
-        boolean car = request.getParameter("car") != null;
+        try {
+            int num = Integer.parseInt(request.getParameter("num"));
+            String language = request.getParameter("language");
+            boolean address = request.getParameter("address") != null;
+            boolean university = request.getParameter("university") != null;
+            boolean country = request.getParameter("country") != null;
+            boolean favColor = request.getParameter("favColor") != null;
+            boolean favAnimal = request.getParameter("favAnimal") != null;
+            boolean email = request.getParameter("email") != null;
+            boolean job = request.getParameter("job") != null;
+            boolean car = request.getParameter("car") != null;
 
-        FakeDataDTO generatedData = fakeDataService.generateFakeData(num, new Locale(language), address, university,
-                country, favColor, favAnimal, email, job, car);
-        model.addAttribute("address", address);
-        model.addAttribute("university", university);
-        model.addAttribute("country", country);
-        model.addAttribute("favColor", favColor);
-        model.addAttribute("favAnimal", favAnimal);
-        model.addAttribute("email", email);
-        model.addAttribute("job", job);
-        model.addAttribute("car", car);
-        model.addAttribute("generatedData", generatedData);
+            FakeDataDTO generatedData = fakeDataService.generateFakeData(num, new Locale(language), address, university,
+                    country, favColor, favAnimal, email, job, car);
+            model.addAttribute("language", language);
+            model.addAttribute("address", address);
+            model.addAttribute("university", university);
+            model.addAttribute("country", country);
+            model.addAttribute("favColor", favColor);
+            model.addAttribute("favAnimal", favAnimal);
+            model.addAttribute("email", email);
+            model.addAttribute("job", job);
+            model.addAttribute("car", car);
+
+            model.addAttribute("headers", headers);
+            model.addAttribute("generatedData", generatedData);
+        }catch(Exception e){
+            model.addAttribute("errorMessage", "Oops, something went wrong:(\nPlease, try again.");
+        }
         return "fakeDataGenerator";
     }
 
+    private Map<String, List<String>> initHeaders() {
+        Map<String, List<String>> map = new HashMap<>();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new ClassPathResource("headers.txt").getInputStream(), StandardCharsets.UTF_8)))
+        {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(":");
+                String language = parts[0].trim();
+                List<String> headers = List.of(parts[1].split(","));
+                map.put(language, headers);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return map;
+    }
 }
