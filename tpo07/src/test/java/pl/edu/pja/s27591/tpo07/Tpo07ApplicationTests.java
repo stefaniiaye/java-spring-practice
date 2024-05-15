7c3d5aaed877f23a0bbdb3af36f91be4ad8febb4
s27591 <s27591@pjwstk.edu.pl>
2024-05-15 12:02:59 +0200

    Added more projects

---
+++ tpo07/src/test/java/pl/edu/pja/s27591/tpo07/Tpo07ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.s27591.tpo07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpo07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
